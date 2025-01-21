4040b377548dff5fe7d500333f9f74f9155fbf9c
mizore-nigit config user.name mizore <2607600595@qq.com>
2025-01-22 00:49:35 +0800

    Site updated: 2025-01-22 00:49:34

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1737478120299";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1737478174561";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
